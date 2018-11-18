017c0a0a20baa98efad0a855a90483aa5cf7ab4a
Marcin Swornowski <swor71@gmail.com>
2018-11-18 13:37:58 +0100

    Add todo form and styling for input

--- src/App.js
+++ src/App.js
@@ -5,6 +5,29 @@ function Todo({ todo }) {
   return <div className="todo">{todo.text}</div>;
 }
 
+function TodoForm({ addTodo }) {
+  const [value, setValue] = useState('');
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (!value) return;
+    addTodo(value);
+    setValue('');
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="input"
+        value={value}
+        onChange={e => setValue(e.target.value)}
+        placeholder="Add a todo"
+      />
+    </form>
+  );
+}
+
 function App() {
   const [todos, setTodos] = useState([
     {
@@ -21,12 +44,18 @@ function App() {
     }
   ]);
 
+  const addTodo = text => {
+    const newTodos = [...todos, { text }];
+    setTodos(newTodos);
+  };
+
   return (
     <div className="app">
       <div className="todo-list">
         {todos.map((todo, index) => (
           <Todo key={index} index={index} todo={todo} />
         ))}
+        <TodoForm addTodo={addTodo} />
       </div>
     </div>
   );
