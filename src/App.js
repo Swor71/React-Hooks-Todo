e601e34720eec2ce4f263df257d420652a7d159d
Marcin Swornowski <swor71@gmail.com>
2018-11-18 13:16:24 +0100

    Add basic working state with useState hook

--- src/App.js
+++ src/App.js
@@ -1,5 +1,35 @@
 import React, { useState } from 'react';
+import './App.css';
 
-function App() {}
+function Todo({ todo }) {
+  return <div className="todo">{todo.text}</div>;
+}
+
+function App() {
+  const [todos, setTodos] = useState([
+    {
+      text: 'New React Hooks',
+      isCompleted: false
+    },
+    {
+      text: 'New React Hooks 2',
+      isCompleted: false
+    },
+    {
+      text: 'New React Hooks 3',
+      isCompleted: false
+    }
+  ]);
+
+  return (
+    <div className="app">
+      <div className="todo-list">
+        {todos.map((todo, index) => (
+          <Todo key={index} index={index} todo={todo} />
+        ))}
+      </div>
+    </div>
+  );
+}
 
 export default App;
